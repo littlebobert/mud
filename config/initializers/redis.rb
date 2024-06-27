95d87d72738bb8e47e3fc6b799297af327aa9929
Justin Garcia <justin.garcia@gmail.com>
2024-06-27 15:41:28 -0400

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 url = ENV["REDISCLOUD_URL"]
 
+$redis = Redis.new(url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
 if url
   Sidekiq.configure_server do |config|
     config.redis = { url: url }
