e0ef948e6bfb4a7dd93fe0a83d65f17514db33aa
Justin Garcia <justin.garcia@gmail.com>
2024-06-28 20:00:44 -0400

    real chime

--- app/jobs/push_afternoon_chime_job.rb
+++ app/jobs/push_afternoon_chime_job.rb
@@ -4,7 +4,15 @@ class PushAfternoonChimeJob < ApplicationJob
   def perform(*args)
     Place.where(outdoors: true).each do |place|
       hash = {
-        outdoor_activity: "<div class=\"outdoor-activity\">you hear the neighborhood 17:00 chime</div>"
+        outdoor_activity: "
+        <div class=\"outdoor-activity\">you hear the neighborhood 17:00 chime</div>
+        <div>
+        <audio controls preload=\"auto\" style=\"\">
+            <source src=\"/neighborhood-chime.m4a\" type=\"audio/mp4\">
+        </audio>
+        </div>
+        ",
+        activity_type: "chime"
       }
       PlaceChannel.broadcast_to(place, hash)
     end
