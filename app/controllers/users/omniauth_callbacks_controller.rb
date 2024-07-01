407e24b2c3a91c72678c1d992f6a3c5dddc148ef
Justin Garcia <justin.garcia@gmail.com>
2024-06-30 21:56:15 -0400

    experiment

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token, only: [:apple]
   
   def apple
     # Verify the callback state
