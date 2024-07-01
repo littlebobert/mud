38fd46653b7d82016da70294a9f7015507ff795a
Justin Garcia <justin.garcia@gmail.com>
2024-06-30 21:14:13 -0400

    experimenting with cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,25 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://www.tokyo-mud.com' : '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    if Rails.env.production?
+      origins 'https://www.tokyo-mud.com', 'https://appleid.apple.com'
+      resource '/users/auth/apple/callback',
+        headers: :any,
+        methods: [:get, :post, :options],
+        credentials: true
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: false
+    else
+      origins 'http://localhost:3000', 'http://127.0.0.1:3000'
+      resource '/users/auth/apple/callback',
+        headers: :any,
+        methods: [:get, :post, :options],
+        credentials: true
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: false
+    end
   end
 end
