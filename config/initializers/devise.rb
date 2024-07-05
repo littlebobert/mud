f13759ff00da309afb5b681609dbcdf3ca1a6017
Justin Garcia <justin.garcia@gmail.com>
2024-07-05 13:43:12 -0400

    save

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -315,9 +315,7 @@ Devise.setup do |config|
     scope: 'email',
     team_id: ENV['APPLE_APP_ID_PREFIX'],
     key_id: ENV['APPLE_KEY_ID'],
-    pem: ENV['APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE'],
-    redirect_uri: ENV['APPLE_REDIRECT_URI'],
-    authorized_client_ids: [ ENV['APPLE_SERVICE_BUNDLE_ID'] ]
+    pem: ENV['APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE']
   }
   
   OmniAuth.config.logger = Rails.logger
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     scope: 'email',
     team_id: ENV['APPLE_APP_ID_PREFIX'],
     key_id: ENV['APPLE_KEY_ID'],
-    pem: ENV['APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE'],
-    redirect_uri: ENV['APPLE_REDIRECT_URI'],
-    authorized_client_ids: [ ENV['APPLE_SERVICE_BUNDLE_ID'] ]
+    pem: ENV['APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE']
+    provider_ignores_state: true
   }
 end
 
