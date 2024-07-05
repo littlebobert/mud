d9708b98538cd73c7ca5438fadfe14ce9cda93cc
Justin Garcia <justin.garcia@gmail.com>
2024-07-05 11:46:23 -0400

    experiment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :apple, ENV['APPLE_SERVICE_BUNDLE_ID'], '', {
-    scope: 'email name',
+    scope: 'email',
     team_id: ENV['APPLE_APP_ID_PREFIX'],
     key_id: ENV['APPLE_KEY_ID'],
     pem: ENV['APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE'],
     redirect_uri: ENV['APPLE_REDIRECT_URI'],
-    authorized_client_ids: [ ENV['APPLE_SERVICE_BUNDLE_ID'] ],
-    client_options: {
-      site: 'https://appleid.apple.com',
-      authorize_url: 'https://appleid.apple.com/auth/authorize',
-      token_url: 'https://appleid.apple.com/auth/token'
-    }
+    authorized_client_ids: [ ENV['APPLE_SERVICE_BUNDLE_ID'] ]
   }
 end
 
