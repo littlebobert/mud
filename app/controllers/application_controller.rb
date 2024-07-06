a523977d61fcb2530a9da72178b4d4665c32bb11
Justin Garcia <justin.garcia@gmail.com>
2024-07-06 18:53:27 -0400

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, only: :apple
   
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
